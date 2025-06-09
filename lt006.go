d8099cebe509b959ade4216e5a55121b1c8836ea
gongyuheng <gong_yuheng@163.com>
2025-06-09 10:45:10 +0800

    Z字形变换

---
+++ lt006.go
@@ -0,0 +1,73 @@
+package algorithm
+
+import (
+	"strings"
+)
+
+/*
+将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+P   A   H   N
+A P L S I I G
+Y   I   R
+之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+请你实现这个将字符串进行指定行数变换的函数：
+string convert(string s, int numRows);
+
+
+示例 1：
+输入：s = "PAYPALISHIRING", numRows = 3
+输出："PAHNAPLSIIGYIR"
+
+示例 2：
+输入：s = "PAYPALISHIRING", numRows = 4
+输出："PINALSIGYAHRPI"
+解释：
+P     I    N
+A   L S  I G
+Y A   H R
+P     I
+
+示例 3：
+输入：s = "A", numRows = 1
+输出："A"
+
+提示：
+
+1 <= s.length <= 1000
+s 由英文字母（小写和大写）、',' 和 '.' 组成
+1 <= numRows <= 1000
+*/
+
+/*
+根据示例：我们可以把布局拆为 2n-2 个字符循环
+P
+A   L
+Y A
+P
+------------
+
+	I
+	S  I
+	H R
+	I
+*/
+func convert(s string, n int) string {
+	length := len(s)
+	if n == 1 || length < n {
+		return s
+	}
+
+	var sb strings.Builder
+	c := 2*n - 2 // 周期步长
+	for i := 0; i < n; i++ {
+		// 依次取字符
+		for j := 0; j+i < length; j = j + c {
+			sb.WriteByte(s[j+i])
+			if i > 0 && i < n-1 && j+c-i < length {
+				sb.WriteByte(s[j+c-i])
+			}
+		}
+	}
+	return sb.String()
+}
